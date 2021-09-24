26b1cae8cafb01abfde11ee3ef096db7335eec4c
Franco Abosso <franco.abosso@gm2dev.com>
2021-09-24 09:28:10 -0300

    parse data by arrays (not strings)

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const fs = require("fs");
-const jsonlines = require("jsonlines");
-const os = require("os");
-const parser = jsonlines.parse();
-let output = [];
+import { readFileSync, appendFileSync } from "fs";
+import { parse } from "jsonlines";
+import { EOL } from "os";
+const parser = parse();
+const output = [];
 
 parser.on("data", (data) => {
+  const line = [];
   if (data.body.length) {
     if (data.body[0].toUpperCase() === "IEEE") {
-      output.push(JSON.stringify(data));
+      line.push("{");
+      Object.entries(data).forEach((entry) => {
+        line.push(JSON.stringify(entry[0]));
+        line.push(":");
+        line.push(JSON.stringify(entry[1]));
+        line.push(",");
+      });
+      line.pop();
+      line.push("}");
+      output.push(line);
     }
   }
 });
 
 let stringified;
 try {
-  stringified = fs.readFileSync("norm_standard_000000000000.jsonl", "utf8");
+  stringified = readFileSync("norm_standard_000000000000.jsonl", "utf8");
 } catch (err) {
   console.error(err);
 } finally {
@@ -23,5 +33,8 @@ try {
 
 parser.end();
 output.forEach((line) => {
-  fs.appendFileSync("./IEEE.jsonl", line + os.EOL);
+  line.forEach((piece) => {
+    appendFileSync("./IEEE.jsonl", piece);
+  });
+  appendFileSync("./IEEE.jsonl", EOL);
 });
